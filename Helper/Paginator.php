202ee26e4eb0b205c443b08661128f88fe644e75
sebastien <sebastien@vaio-vpcf1>
2013-10-14 18:21:54 +0200

    Working on flexstore (tests)

--- Helper/Paginator.php
+++ Helper/Paginator.php
@@ -5,14 +5,22 @@
  */
 
 namespace Soluble\FlexStore\Helper;
-
+use Soluble\FlexStore\Exception;
 use Zend\Paginator\Paginator as ZendPaginator;
 
 class Paginator extends ZendPaginator
 {
 
 
-	function __construct($totalRows, $limit, $offset) {
+	function __construct($totalRows, $limit, $offset=0) {
+		
+		if (!is_integer($limit)) {
+			throw new Exception\InvalidUsageException(__FUNCTION__ . ' expects limit to be an integer');
+		}
+		if ($limit < 1) {
+			throw new Exception\InvalidUsageException(__FUNCTION__ . ' expects limit to be an integer greater than 0');
+		}
+		
 		
 		$adapter = new \Zend\Paginator\Adapter\Null($totalRows);
 		parent::__construct($adapter);

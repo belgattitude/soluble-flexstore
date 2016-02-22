b25e37bb7f074f5a8133f1895667290287081910
Sébastien Vanvelthem <belgattitude@gmail.com>
2016-02-22 17:48:12 +0100

    Support for ZF3 servicemanager in Paginator, close #2

--- src/Soluble/FlexStore/Helper/Paginator.php
+++ src/Soluble/FlexStore/Helper/Paginator.php
@@ -9,8 +9,23 @@ namespace Soluble\FlexStore\Helper;
 use Soluble\FlexStore\Exception;
 use Zend\Paginator\Paginator as ZendPaginator;
 
+use Zend\Paginator\ScrollingStyle;
+
 class Paginator extends ZendPaginator
 {
+    
+    /**
+     * Default set of scrolling
+     *
+     * @var array
+     */
+    protected $scrollingTypes = array(
+        'all'     => 'Zend\Paginator\ScrollingStyle\All',
+        'elastic' => 'Zend\Paginator\ScrollingStyle\Elastic',
+        'jumping' => 'Zend\Paginator\ScrollingStyle\Jumping',
+        'sliding' => 'Zend\Paginator\ScrollingStyle\Sliding',
+    );    
+    
     /**
      *
      * @param integer $totalRows
@@ -47,5 +62,54 @@ class Paginator extends ZendPaginator
         parent::__construct($adapter);
         $this->setItemCountPerPage($limit);
         $this->setCurrentPageNumber(ceil(($offset + 1) / $limit));
+        
     }
+    
+    
+    /**
+     * Loads a scrolling style.
+     *
+     * @param string $scrollingStyle
+     * @return ScrollingStyleInterface
+     * @throws Exception\InvalidArgumentException
+     */
+    protected function _loadScrollingStyle($scrollingStyle = null)
+    {
+        if ($scrollingStyle === null) {
+            $scrollingStyle = static::$defaultScrollingStyle;
+        }
+
+        switch (strtolower(gettype($scrollingStyle))) {
+            case 'object':
+                if (!$scrollingStyle instanceof ScrollingStyleInterface) {
+                    throw new Exception\InvalidArgumentException(
+                        'Scrolling style must implement Zend\Paginator\ScrollingStyle\ScrollingStyleInterface'
+                    );
+                }
+
+                return $scrollingStyle;
+
+            case 'string':
+                if (!array_key_exists(strtolower($scrollingStyle), $this->scrollingTypes)) {
+                    throw new Exception\InvalidArgumentException(
+                        "Scrolling type '$scrollingStyle' is not supported, look for (" . 
+                        join(',', array_keys($this->scrollingTypes)) .
+                        ")"    
+                    );
+                }
+                $cls = $this->scrollingTypes[strtolower($scrollingStyle)];
+                return new $cls();
+                
+
+            case 'null':
+                // Fall through to default case
+
+            default:
+                throw new Exception\InvalidArgumentException(
+                    'Scrolling style must be a class ' .
+                    'name or object implementing Zend\Paginator\ScrollingStyle\ScrollingStyleInterface'
+                );
+        }
+    }
+    
 }

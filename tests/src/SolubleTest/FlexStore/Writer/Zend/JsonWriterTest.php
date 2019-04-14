9ff21cd6e23234b104e0ced65eb86f09c4016b72
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-04-14 20:25:42 +0200

    remove teardown method

--- tests/src/SolubleTest/FlexStore/Writer/Zend/JsonWriterTest.php
+++ tests/src/SolubleTest/FlexStore/Writer/Zend/JsonWriterTest.php
@@ -53,14 +53,6 @@ class JsonWriterTest extends TestCase
         $this->jsonWriter->setStore(new FlexStore($this->source));
     }
 
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown()
-    {
-    }
-
     public function testGetData()
     {
         $data = $this->jsonWriter->getData();

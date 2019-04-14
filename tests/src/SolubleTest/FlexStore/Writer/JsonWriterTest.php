feb0519487e544d8a3de8bf2719fc4674867416a
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-04-14 20:25:29 +0200

    remove teardown method

--- tests/src/SolubleTest/FlexStore/Writer/JsonWriterTest.php
+++ tests/src/SolubleTest/FlexStore/Writer/JsonWriterTest.php
@@ -57,16 +57,6 @@ class JsonWriterTest extends TestCase
         $this->jsonWriter->setStore(new FlexStore($this->source));
     }
 
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown()
-    {
-        //header_remove();
-        parent::tearDown();
-    }
-
     public function testGetData()
     {
         $data = $this->jsonWriter->getData();
